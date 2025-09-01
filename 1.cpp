4fddf61861d6c72f447f269c8a8902cf8da9ed39
rajeshwar <iitkgprajeshwar@gmail.com>
2025-09-01 22:46:31 +0530

    1st commit

---
+++ 1.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    multiset<int> v1;
+    vector<int> v2(m);
+    for (int i = 0; i < n; i++)
+    {
+        int ticket;
+        cin >> ticket;
+        v1.insert(ticket);
+    }
+    for (int i = 0; i < m; i++)
+    {
+        cin >> v2[i];
+    }
+    // sort(v1.begin(),v1.end());
+    for (auto v : v2)
+    {
+        auto it = v1.upper_bound(v);
+
+        if (it == v1.begin())
+        {
+            cout << -1 << "\n";
+        }
+        else
+        {
+            it--;
+            cout << *it << "\n";
+            v1.erase(it);
+        }
+    }
+}
